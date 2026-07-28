3a21f653ef041d2189d4998cff9c7550c737e748
mrc-rdn <johnmarcoardina49@gmail.com>
2026-07-28 21:28:27 +0800

    a

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className='flex flex-col items-center w-full pt-30 '>
         <div className='w-6/12 flex'>
-            <img src="public/images/Mrc.png" alt="" className='w-70' />
+            <img src="/images/Mrc.png" alt="" className='w-70' />
             <div className='text-gray-400'>
                 <h1 className='text-gray-100 font-bitcount text-3xl px-10'>John Marco Ardina</h1>
                 <p className=" font-display font-saira text-lg  text-[15px] px-10 py-3">
