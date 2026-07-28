28d09cc915d84bd59578111d040b0823de19ec9b
mrc-rdn <johnmarcoardina49@gmail.com>
2026-07-28 19:55:07 +0800

    done

--- src/Routes/AppRoutes.tsx
+++ src/Routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import {Routes, Route} from 'react-router-dom'
 import PublicLayout from '../layouts/publiclayout/PublicLayout.tsx'
 import Home from '../features/Home/Home.tsx'
