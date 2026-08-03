8eb0d5524ffa25418f6fb03ec2a40a343883bfeb
mrc-rdn <johnmarcoardina49@gmail.com>
2026-08-03 23:22:45 +0800

    small tweak color background

--- src/layouts/publiclayout/PublicLayout.tsx
+++ src/layouts/publiclayout/PublicLayout.tsx
@@ -28,7 +28,7 @@ export default function PublicLayout() {
         <div className='lg:flex bg-neutral-900 min-h-screen text-gray-200'>
 
             {/* Desktop Sidebar */}
-            <div className='h-screen w-50 bg-neutral-800 border-r border-gray-500 hidden lg:flex flex-col shrink-0'>
+            <div className='h-screen w-50 bg-neutral-900 border-r border-gray-500 hidden lg:flex flex-col shrink-0'>
                 <div className='px-8 py-5 mb-auto'>
                     <Link to="/" className='text-white text-2xl font-bitcount mb-7'>Mrco</Link>
                     {
@@ -53,7 +53,7 @@ export default function PublicLayout() {
             </div>
 
             {/* Mobile Overlay Sidebar */}
-            <div className={isOpen ? 'h-screen w-full bg-neutral-800 border-r border-gray-500 fixed lg:hidden z-50 flex flex-col' : 'h-screen w-full bg-neutral-800 border-r border-gray-500 fixed hidden lg:hidden z-50 flex flex-col '}>
+            <div className={isOpen ? 'h-screen w-full bg-neutral-900 border-r border-gray-500 fixed lg:hidden z-50 flex flex-col' : 'h-screen w-full bg-neutral-800 border-r border-gray-500 fixed hidden lg:hidden z-50 flex flex-col '}>
                 <div className='h-15 flex items-center border-b border-gray-500 py-3 px-10 shrink-0'>
                     <h2 className='text-white text-2xl font-bitcount'>{'</>Mrc'}</h2>
                     <button
@@ -88,7 +88,7 @@ export default function PublicLayout() {
             </div>
 
             {/* Mobile Top Header */}
-            <div className='h-15 w-full bg-neutral-800 border-b border-gray-500 px-10 flex justify-between items-center lg:hidden shrink-0'>
+            <div className='h-15 w-full bg-neutral-900 border-b border-gray-500 px-10 flex justify-between items-center lg:hidden shrink-0'>
                 <h2 className='text-white text-2xl font-bitcount'>{'</>Mrc'}</h2>
                 <button
                     className="lg:hidden text-white text-2xl font-bitcount cursor-pointer"
@@ -99,7 +99,7 @@ export default function PublicLayout() {
             </div>
 
             {/* Main Scrollable Area */}
-            <main className='bg-neutral-800 h-screen w-full overflow-y-auto relative bg-grid-pattern'>
+            <main className='bg-neutral-900 h-screen w-full overflow-y-auto relative bg-grid-pattern'>
                 {/* Subtle ambient glows for visual depth */}
                 <div className='absolute top-0 right-0 w-[300px] sm:w-[500px] h-[300px] sm:h-[500px] bg-white/5 rounded-full blur-[100px] sm:blur-[120px] pointer-events-none z-0' />
                 <div className='absolute bottom-0 left-0 w-[400px] sm:w-[600px] h-[400px] sm:h-[600px] bg-neutral-500/5 rounded-full blur-[120px] sm:blur-[140px] pointer-events-none z-0' />
